659c8d482c0e150929c0b39e4e550a23f28998ec
TANAKARYOTA <126657386+TANAKARYOTAlife@users.noreply.github.com>
2023-08-18 21:04:40 +0900

    Delete SincPython - 2023.7.10 - directory

--- SincPython - 2023.7.10 -/Sinc_Mimetype.py
+++
@@ -1,9 +0,0 @@
-#import
-import os
-
-#Sinc_Mimetype_Function
-def Sinc_Mimetype():
-    with open( 'mimetype' , 'w' ) as Sinc_Mimetype_File:
-        Sinc_Mimetype_File.write('application/epub+zip')
-
-Sinc_Mimetype()
